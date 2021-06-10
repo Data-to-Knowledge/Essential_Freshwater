7dfe21a88d6c50a731434e803da78bf40592ff35
kurtvanness <kurt.vanness@ecan.govt.nz>
2021-06-11 10:37:28 +1200

    GW Indicators script updates

---
+++ water_quality/python/Indicator Results Script/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+#-Authorship information-###################################################################
+__author__ = 'Kurt van Ness'
+__copyright__ = 'Environment Canterbury'
+__version__ = '1.0'
+__email__ = 'kurt.vanness@ecan.govt.nz'
+__date__ ='June 2021'
+############################################################################################
